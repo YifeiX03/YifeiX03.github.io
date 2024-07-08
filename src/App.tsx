e5386c744c335b0df318e99caae7a0b0cef72e35
UBC Student <stdntyx@students.cs.ubc.ca>
2024-07-07 21:57:44 -0700

    Added buttons, now you can add to counter with buttons

--- src/App.tsx
+++ src/App.tsx
@@ -20,19 +20,51 @@ function Greet(props: any) {
   );
 }
 
+const Display = ({ counter }: any) => <div>{counter}</div>;
+const Button = ({ onClick, text }: any) => (
+  <button onClick={onClick}>{text}</button>
+);
+
+const AddForm = ({ func, amount }: any) => {
+  const [value, setValue] = useState(0);
+  function addAmount(num: number) {
+    if (!isNaN(num)) {
+      func(num);
+    }
+  }
+  return (
+    <div>
+      <input
+        onChange={(e) => setValue(parseInt(e.target.value))}
+        type="number"
+      />
+      <button onClick={() => addAmount(amount + value)}>Add {value}</button>
+    </div>
+  );
+};
+
+console.log("Hello");
+
 function App() {
-  console.log("Hello");
   const [counter, setCounter] = useState(0);
-
-  setTimeout(() => setCounter(counter + 1), 1000);
-
+  const setToValue = (newValue: any) => () => {
+    console.log("value now: ", newValue);
+    setCounter(newValue);
+  };
+  // setTimeout(() => setCounter(counter + 1), 1000);
+  // const increaseByOne = () => setCounter(counter + 1);
+  // const decreaseByOne = () => setCounter(counter - 1);
+  // const setToZero = () => setCounter(0);
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Yippee Epic Website</h1>
+        <h1>Epic Website</h1>
         <Hello />
-        <Greet name="Bob" />
-        <div>{counter}</div>
+        <Display counter={counter} />
+        <Button onClick={setToValue(counter + 1)} text="+1" />
+        <Button onClick={setToValue(counter - 1)} text="-1" />
+        <Button onClick={setToValue(0)} text="0" />
+        <AddForm func={setCounter} amount={counter} />
       </header>
     </div>
   );
